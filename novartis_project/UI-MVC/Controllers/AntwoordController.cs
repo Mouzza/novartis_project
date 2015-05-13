22ca5284986a89c6cce6ccef8c6fde48efc7286d
MichielReenaersKdg <michiel-reenaers@hotmail.com>
2015-05-13 22:53:27 +0200

    Lol

--- novartis_project/UI-MVC/Controllers/AntwoordController.cs
+++ novartis_project/UI-MVC/Controllers/AntwoordController.cs
@@ -126,9 +126,29 @@ namespace JPP.UI.Web.MVC.Controllers
             return View();
         }
 
-        public ActionResult DossierModelOne()
+        public ActionResult DossierModelOne(DossierAntwoord dossierAntwoord)
         {
-            return View();
+            if (dossierAntwoord.titel != null)
+            {
+                return View(dossierAntwoord);
+            }
+            else
+            {
+
+                dossierAntwoord = new DossierAntwoord()
+                {
+
+                    titel = "Geef titel",
+                    subtitel = "Geef subtitel",
+                    inhoud = "Aliquam condimentum magna ac ultricies posuere. Cras viverra velit lectus,vel pretium nulla posuere sit amet. Vestibulum venenatis volutpat dui. Aliquam dictum metus eget est sodales malesuada. Nunc pharetra iaculis suscipit. Mauris sed lectus nec nunc laoreet molestie et ac ex. Duis a aliquam sapien. Nullam fermentum diam arcu, nec lacinia metus pulvinar at. Nunc eget tempor ex. Nunc vehicula neque ut vulputate feugiat. Aenean euismod posuere nunc, a aliquet nunc laoreet nec. Phasellus faucibus mi et bibendum pretium. Morbi magna lorem, eleifend at convallis quis, pretium id turpis. In suscipit, magna ac laoreet pellentesque, augue risus cursus arcu, eget ornare est libero vel leo. Etiam hendrerit hendrerit arcu, posuere semper sapien facilisis a.",
+                    textvak2 = "Aliquam condimentum magna ac ultricies posuere. Cras viverra velit lectus,vel pretium nulla posuere sit amet. Vestibulum venenatis volutpat dui. Aliquam dictum metus eget est sodales malesuada. Nunc pharetra iaculis suscipit. Mauris sed lectus nec nunc laoreet molestie et ac ex. Duis a aliquam sapien. Nullam fermentum diam arcu, nec lacinia metus pulvinar at. Nunc eget tempor ex. Nunc vehicula neque ut vulputate feugiat. Aenean euismod posuere nunc, a aliquet nunc laoreet nec. Phasellus faucibus mi et bibendum pretium.",
+                    textvak3 = "Aliquam condimentum magna ac ultricies posuere. Cras viverra velit lectus,vel pretium nulla posuere sit amet. Vestibulum venenatis volutpat dui. Aliquam dictum metus eget est sodales malesuada. Nunc pharetra iaculis suscipit. Mauris sed lectus nec nunc laoreet molestie et ac ex. Duis a aliquam sapien. Nullam fermentum diam arcu, nec lacinia metus pulvinar at. Nunc eget tempor ex. Nunc vehicula neque ut vulputate feugiat. Aenean euismod posuere nunc, a aliquet nunc laoreet nec. Phasellus faucibus mi et bibendum pretium.",
+                    afbeeldingPath = "~/uploads/379465.png"
+
+                };
+
+                return View(dossierAntwoord);
+            }
         }
 
         public ActionResult DossierModelTwo()
