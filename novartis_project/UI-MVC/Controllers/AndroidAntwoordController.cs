d899781f81049647c9641b57519d3d29d8ca542a
batb <mergenbat@gmail.com>
2015-05-22 15:58:02 +0200

    api stem POST

--- novartis_project/UI-MVC/Controllers/AndroidAntwoordController.cs
+++ novartis_project/UI-MVC/Controllers/AndroidAntwoordController.cs
@@ -591,7 +591,7 @@ namespace JPP.UI.Web.MVC.Controllers
         #endregion
 
         #region STEM FLAG
-        [HttpGet]
+        [HttpPost]
         [ActionName("stemAntwoord")]
         public IHttpActionResult stemAntwoord(ANDROIDstem aStem)
         {
@@ -621,7 +621,7 @@ namespace JPP.UI.Web.MVC.Controllers
 
 
         }
-        [HttpGet]
+        [HttpPost]
         [ActionName("flagAntwoord")]
         public IHttpActionResult flagAntwoord(ANDROIDFlag aFlag)
         {
